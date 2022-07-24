e8a6fe132c726dcc663e50337adf1077a507348a
swap2425 <108945586+swap2425@users.noreply.github.com>
2022-07-24 14:50:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,9 @@ st.header('User Input Parameters')
 
 num1=st.number_input("Enter one number")
 num2=st.number_input("Enter another number")
-if num2==0:
-  st.write('Division by Zero not possible')
-else:
+try:
   res=num1/num2
-
   st.subheader('Answer')
-
   st.write(res)
-
+except ZeroDivisionError:
+    st.write("Division by Zero!")
